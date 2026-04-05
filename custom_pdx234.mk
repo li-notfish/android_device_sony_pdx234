460e3f76cd45ae298bfc60dc279bdae769b45e7b
onlyfish <linotfish@outlook.com>
2026-04-06 00:53:50 +0800

    pdx234: Update blobs to XQ-DQ72_Customized_SEA-67.2.A.3.163

--- custom_pdx234.mk
+++ custom_pdx234.mk
@@ -39,8 +39,8 @@ PRODUCT_BUILD_PROP_OVERRIDES := \
     DeviceProduct=XQ-DQ72 \
     SystemDevice=XQ-DQ72 \
     SystemName=XQ-DQ72 \
-    BuildFingerprint=Sony/XQ-DQ72/XQ-DQ72:15/67.2.A.3.141/067002A003014100521143226:user/release-keys \
-    BuildDesc="XQ-DQ72-user 15 67.2.A.3.141 067002A003014100521143226 release-keys"
+    BuildFingerprint=Sony/XQ-DQ72/XQ-DQ72:15/67.2.A.3.163/067002A003016300521143226:user/release-keys \
+    BuildDesc="XQ-DQ72-user 15 67.2.A.3.163 067002A003016300521143226 release-keys"
 
 PRODUCT_NAME := custom_pdx234
 PRODUCT_DEVICE := pdx234
